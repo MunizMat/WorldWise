5630afa980c29459993a982e03028e6b99512554
Matheus Muniz <matheusmuniz215@gmail.com>
2024-09-27 20:45:25 -0300

    chore: remove unused packages

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,27 +1,28 @@
-import '@mantine/core/styles.css';
+/* ----------- External ----------- */
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
+
+/* ----------- Theme ----------- */
 import { theme } from '../theme';
+
+/* ----------- Assets ----------- */
 import '../index.css';
-import '@mantine/dates/styles.css';
+import '@mantine/core/styles.css';
 import '@mantine/charts/styles.css';
-import { ModalsProvider } from '@mantine/modals';
 
 export default function App({ Component, pageProps }: any) {
   return (
     <MantineProvider theme={theme}>
-      <ModalsProvider>
-        <Head>
-          <title>WorldWise</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-          />
-          <link rel="shortcut icon" href="/favicon.svg" />
-        </Head>
+      <Head>
+        <title>WorldWise</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
 
-        <Component {...pageProps} />
-      </ModalsProvider>
+      <Component {...pageProps} />
     </MantineProvider>
   );
 }
